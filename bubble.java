1ebd4c41a768a04c710982086517eaf9d87618e4
Sameeksha07-pura <98949863+Sameeksha07-pura@users.noreply.github.com>
2022-07-06 16:39:19 +0530

    Add files via upload

---
+++ bubble.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class bubble {
+    public static void main(String[] args) {
+     int a[],b[],i;
+     a=new int[10];
+     b=new int[10];
+     Scanner sc=new Scanner(Sytem.in);
+     System.out.println("Enter array values");
+     for(i=0;i<10;i++)
+     a[i]=sc.nextInt();
+
+    }
+}
